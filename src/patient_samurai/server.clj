600e827c18d4d450a1203d9724461f10ebcc4c9d
Nikita Levchuk <code@nikitalevchuk.me>
2022-09-04 15:05:32 +0200

    feature: add index page with mock data

--- src/patient_samurai/server.clj
+++ src/patient_samurai/server.clj
@@ -1,16 +1,93 @@
 (ns patient-samurai.server
   (:require
-      [ring.adapter.jetty :refer [run-jetty]])
+    [compojure.core :as compojure]
+    [compojure.route :as route]
+    [rum.core :as rum]
+    [ring.adapter.jetty :refer [run-jetty]]
+    [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
   (:gen-class))
 
-(defn handler [req]
-  {:status 200
-   :headers {"Content-Type" "text/plain; charset=UTF-8"}
-   :body (str "Hello World! \n (uri = " (get req :uri) ")")})
+(defonce patients
+  (atom [{:id 1
+          :fname "Roman"
+          :mname "Andreevich"
+          :lname "Suteev"
+          :sex "Male"
+          :birth-date "2000-01-03"
+          :address {:country "Montenegro"
+                    :city "Budva"
+                    :street "Jadranski Put"
+                    :number 11}
+          :insurance "GH312DS"}
+         {:id 2
+          :fname "Olga"
+          :mname "Fedorovna"
+          :lname "Dostoevskaya"
+          :sex "Female"
+          :birth-date "1995-05-24"
+          :address {:country "Montenegro"
+                    :city "Tivat"
+                    :street "21 Novembra"
+                    :number 44}
+          :insurance "ELV5KV"}]))
+
+(defn format-address [{:keys [country, city, street, number]}]
+  (str country ", " city ", " street " " number))
+
+(rum/defc patient-row [patient]
+  (let [{:keys [id fname mname lname sex birth-date address insurance]} patient]
+    [:tr {:key id}
+      [:td (str fname " " mname " " lname)]
+      [:td sex]
+      [:td birth-date]
+      [:td (format-address address)]
+      [:td insurance]]))
+
+(rum/defc patient-list [patients]
+  [:<>
+   [:header
+    [:h1 "Patient List"]]
+   [:main
+    [:table
+      [:thead
+       [:tr
+        [:th "Full Name"]
+        [:th "Sex"]
+        [:th "Birth Date"]
+        [:th "Address"]
+        [:th "Insurance Number"]]]
+      [:tbody
+        (for [patient patients]
+         (patient-row patient))]]]])
+
+(rum/defc index []
+  [:html {:lang "en"}
+   [:head
+    [:title "Patient Samurai"]
+    [:meta {:charset "UTF-8"}]
+    [:meta {:name "viewport"
+            :content "width=device-width, initial-scale=1"}]
+    [:link {:href "styles.css"
+            :rel "stylesheet"
+            :type "text/css"}]]
+   [:body
+    [:div#app
+     (patient-list @patients)]
+    [:script {:src "main.js" :type "text/javascript" }]]])
+
+(compojure/defroutes routes
+  (compojure/GET "/" [] (str "<!DOCTYPE html>" (rum/render-html (index))))
+  ; (compojure/GET "/new" [] ...)
+  ; (compojure/GET "/edit/:id" [] ...)
+  ; (compojure/POST "/edit/:id" [] ...)
+  ; (compojure/DELETE "/:id" [] ...)
+  (route/not-found "Not Found"))
 
 (defn -main [& args]
-  (run-jetty #'handler {:port 3000
-                        :join? false}))
+  (run-jetty
+    (wrap-defaults #'routes site-defaults)
+    {:port 3000
+     :join? false}))
 
 (comment
   (def server (-main))
